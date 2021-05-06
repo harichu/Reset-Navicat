8529548b6b17f6e87881ce71c95b7ddd3c8af6f8
HARICHU <chuhainet@gmail.com>
2021-05-06 12:26:45 +0700

    Update Navicat无限试用.bat

--- Navicat无限试用.bat
+++ Navicat无限试用.bat
@@ -1,19 +1,19 @@
 @color a0
-@title 重置Navicat试用期限
+@title Reset Navicat trial period
 @echo off
 echo --------------------------
-echo     正在关闭进程...
+echo     Closing process...
 echo --------------------------
 taskkill /F /T /IM navicat.exe
 if exist %temp%\backup.reg (
 del %temp%\backup.reg
 )
 echo --------------------------
-echo     正在备份注册表...
+echo     Backing up the registry...
 echo --------------------------
 reg EXPORT HKCU\Software %temp%\backup.reg
 echo --------------------------
-echo   正在生成注册表文件...
+echo   Generating registry file...
 echo --------------------------
 set fr=-
 echo Windows Registry Editor Version 5.00 >> %temp%\Navicat.reg
@@ -22,11 +22,11 @@ for /F "delims=" %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" 
 echo [%fr%%%i] >> %temp%\Navicat.reg
 )
 echo --------------------------
-echo     正在执行操作...
+echo     Operation in progress...
 echo --------------------------
 regedit /s %temp%\Navicat.reg
 echo --------------------------
-echo   Navicat试用期已重置...
+echo   Navicat trial period has been reset...
 echo --------------------------
 del %temp%\Navicat.reg
 pause
